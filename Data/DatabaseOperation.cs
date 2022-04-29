f27876cb33b5cdbdc42ea3362d20e2db88de8142
wsi7 <collegenik@outlook.com>
2022-04-29 16:46:03 +0300

    Добавьте файлы проекта.

---
+++ Data/DatabaseOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Data;
+
+namespace Demo
+{
+    class DatabaseOperation
+    {
+        public static class DatabaseOperations
+        {
+
+            private static bd7Entities Entities;
+
+
+            public static void InitializeEntities()
+            {
+
+                if (Entities is null)
+                {
+                    Entities = new bd7Entities();
+                }
+
+            }
+        }
+    }
+}
---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+
+            InitializeComponent();   
+            DataContext = this;
+
+        }
+
+        private void search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (search.Text == "Введите для поиска")
+            {
+                search.Text = "";
+            }
+        }
+
+        private void search_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (search.Text == "")
+            {
+                search.Text = "Введите для поиска";
+            }
+        }
+    }
+}
